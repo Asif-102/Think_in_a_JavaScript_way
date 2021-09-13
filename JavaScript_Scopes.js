5936759710115d8bd0ddc24ea9e0cf8c29c737b1
Asif-102 <aiqbal517@gmail.com>
2021-09-14 02:17:38 +0600

    JavaScript Scopes

--- JavaScript_Scopes.js
+++ JavaScript_Scopes.js
@@ -1,8 +1,13 @@
-var x = 23;
+"user strict" // use strick used for stop naming conflict
+
+var x = 23; // this is window.x
 
 function myFunc(){
-    var y = 10;
+    var x = 10; // this is myFunc.x
+    // x = 10 <= this is also change global variable and this is highly risky
+    console.log(x, 'from myFunc()');
 }
 
+myFunc();
+
 console.log(x);
-console.log(y);
